1c48528dfa45d971ab33c6789b832c3f699b0ac7
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 12:32:17 +0600

    variable declaration policy is added in this policy

---
+++ variable-name.js
@@ -0,0 +1,32 @@
+var name = 'Jahangir';
+
+// In JavaScript, You can not use their reserved keywords as your variable name 
+
+// var false = 15;
+// var delete = 'delete';
+
+// name should be one word 
+// var nayok name = 'Tom Hanks';
+
+
+// Quote is not allowed 
+// var "nayika" = "Shabnur";
+
+
+// var my-office - address = 'New York';
+var my_office_address = 'New York';
+var myOfficeAddress = 'New York';
+var myofficeaddress = 'New York';
+
+var PIE = 3.14;
+
+
+// var 999price = 25;
+var price99 = 55;
+var p45 = 96;
+
+// var userHomeAddress = 'Katabon';
+// var uha = 'Dhaka';
+
+
+console.log(name, p45);
