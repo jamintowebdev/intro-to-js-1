c2f69b6e38349290e588e26067372361b9e1e2b5
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 12:05:13 +0600

    variable file added string and boolean type variable and done some output with console.log

--- variable.js
+++ variable.js
@@ -1,5 +1,9 @@
 // vary + able = variable (changeable)
 
+
+
+// Integar Variable : Number type
+
 var price = 21;
 var age = 13;
 var temperature = 37;
@@ -9,4 +13,21 @@ var village = 'Gorma';
 var postOffice = 'Barhatta';
 var district = 'Netrokona';
 
+// name declaration with letters
+// ********* String Type Variable **********
+var myName = 'Jahangir Alom Minto';
+var address = 'Gorma near Barhatta Degree College';
+var gf = 'Adiba Rahman Eisha';
+var myMessage = 'Jahangir Alom Minto loves you so much';
+var school = 'Kashbon Biddya Niketon High School';
+
+
+// Boolean type Variable ; need not quotaion marks
+
+var pass = true;
+var subscribed = false;
+
+ 
 console.log(age, born);
+console.log(gf + ' + ' + myName);
+console.log(myMessage);
