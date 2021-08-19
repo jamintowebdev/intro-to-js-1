47cc4a4a834dc612fb11e956819f1f13c016f6bf
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 15:33:14 +0600

    final commit for the intro-to-js-1 module

---
+++ summary.js
@@ -0,0 +1,30 @@
+// number type variable 
+var eggPrice = 39;
+eggPrice = 39 * 2;
+console.log(eggPrice);
+console.log(typeof eggPrice);
+
+// string type variable 
+var webDeveloper = 'jamintowebdev';
+
+
+// boolean type variable 
+var isHot = true;
+
+
+// Modulus 
+var mangoes = 7;
+var hungryPerson = 2;
+var remainder = mangoes % hungryPerson;
+console.log(remainder);
+
+// Modulus 
+var mangoes = 57;
+var hungryPerson = 12;
+var remainder = mangoes % hungryPerson;
+var dividing = mangoes / hungryPerson;
+console.log(dividing);
+
+
+
+
