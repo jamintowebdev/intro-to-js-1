d4d6121252c30803f822c7fff198d44ff0421ab2
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 15:21:00 +0600

    final commit for the js conversion file

---
+++ variable-types.js
@@ -0,0 +1,97 @@
+var tShirtPrice = 200;
+console.log(tShirtPrice);
+
+// now with the type of
+var tShirtPrice = 200;
+console.log(typeof tShirtPrice);
+
+
+// now with the type of
+var tShirt = 'Smoke White';
+console.log(typeof tShirt);
+
+// without type of
+var isRaining = true;
+console.log(isRaining);
+
+// with type of
+var isRaining = true;
+console.log(typeof isRaining);
+
+var isRomantic;
+console.log(isRomantic);
+
+
+var today = new Date();
+console.log(today);
+
+// with type of
+var today = new Date();
+console.log(typeof today);
+
+
+// ******* special edition *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+console.log(total);
+
+
+// ******* special edition - 01 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+console.log(total.toFixed(1));
+
+// ******* special edition - 01 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+console.log(total.toFixed(5));
+
+
+// ******* special edition - 01 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+//converting into integer with parseInt()
+console.log(parseInt(total.toFixed(5)));
+
+
+// ******* special edition - 02 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+//converting into integer with parseFloat()
+console.log(parseFloat(total.toFixed(5)));
+
+
+// ******* special edition - 03 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total = total.toFixed(1);
+//converting into integer with parseInt()
+console.log(total);
+
+
+
+// ******* special edition - 04 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+// total = total.toFixed(1);
+console.log(total);
+
+
+// ******* special edition - 04 *********
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total = total.toFixed(1);
+total = parseFloat(total);
+console.log(total);
+
+
+
+
