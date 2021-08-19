8cc4e3d669ad65f72e73918b597c75fda7ec3895
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 12:49:59 +0600

    js mathematical operation learned and practiced in this js file

---
+++ mathematical-operation.js
@@ -0,0 +1,22 @@
+var onionPrice = 43;
+var eggPrice = 10;
+
+var totalPrice = onionPrice + eggPrice;
+
+var priceDifference = onionPrice - eggPrice;
+
+var eggQuantity = 7;
+var totalEggPrice = eggQuantity * eggPrice;
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice + eggPrice);
+console.log(totalPrice);
+console.log(priceDifference);
+console.log(totalEggPrice);
+
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice;
+
+console.log(orangeQuantity);
