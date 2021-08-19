b8678ee6b2859d03a326dde97d2143c3d2e701ed
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-19 11:42:07 +0600

    first commit specially for the JavaScript Programming and Learning and Practicing here

---
+++ variable.js
@@ -0,0 +1,12 @@
+// vary + able = variable (changeable)
+
+var price = 21;
+var age = 13;
+var temperature = 37;
+
+var born = 1993;
+var village = 'Gorma';
+var postOffice = 'Barhatta';
+var district = 'Netrokona';
+
+console.log(age, born);
